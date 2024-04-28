f840c3397285623289f435e38c25f11e5c591771
mazc <mazc@dibo.ltd>
2024-04-28 15:19:28 +0800

    * 优化package，添加license头等

--- diboot-ai-starter/src/main/java/com/diboot/starter/AiAutoConfig.java
+++ diboot-ai-starter/src/main/java/com/diboot/starter/AiAutoConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020, www.dibo.ltd (service@dibo.ltd).
+ * Copyright (c) 2015-2029, www.dibo.ltd (service@dibo.ltd).
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.diboot.ai.starter;
+package com.diboot.starter;
 
 import com.diboot.ai.client.AiClient;
 import com.diboot.ai.config.AiConfiguration;
