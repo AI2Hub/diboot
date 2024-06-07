58c176e452985d4e9e830dbcea9d4b4c1b96f038
mazc <mazc@dibo.ltd>
2024-06-07 11:30:17 +0800

    * 更新多租户组件初始化顺序

--- diboot-tenant-starter/src/main/java/com/diboot/tenant/init/TenantPluginInitializer.java
+++ diboot-tenant-starter/src/main/java/com/diboot/tenant/init/TenantPluginInitializer.java
@@ -29,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -38,6 +39,7 @@ import org.springframework.stereotype.Component;
  * @version : v3.2.0
  * @Date 2023/12/18
  */
+@Order(971)
 @Slf4j
 @Component
 @ConditionalOnProperty(prefix = "diboot", name = "init-sql", havingValue = "true")
